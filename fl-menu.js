49ef2551654cd52634ef404aa16453257e933b8b
hesslau <hesslau@gmail.com>
2015-09-14 16:33:29 -0700

    add comments and code cleanup

--- fl-menu.js
+++ fl-menu.js
@@ -1,5 +1,14 @@
 (function() {
 
+    // set the entries { title: url }
+    var entries = {
+      "Positive Social Action Hero Missions": "http://focallocal.org",
+      "Community News": "http://focallocal.org",
+      "What is the Focallocal Movement?": "http://focallocal.org",
+      "Events": "http://focallocal.org",
+      "Shop": "http://focallocal.org",
+    }
+
   var loadCss = function(cssPath) {
     link = document.createElement( "link" );
     link.href = cssPath;
@@ -18,12 +27,12 @@
     return div;
   }
 
+  // add the fl-menu to the body as first child
   var menu = document.createElement("div");
   menu.id = "fl-menu";
-
   document.body.insertBefore(menu, document.body.firstChild);
 
-
+  // add container for links to the menu
   var container = document.createElement("div");
   container.className = "menu-container row";
   menu.appendChild(container);
@@ -33,15 +42,7 @@
   logo.className = "menu-logo col s1";
   container.appendChild(logo);
 
-  // add the entries
-  var entries = {
-    "Positive Social Action Hero Missions": "http://focallocal.org",
-    "Community News": "http://focallocal.org",
-    "What is the Focallocal Movement?": "http://focallocal.org",
-    "Events": "http://focallocal.org",
-    "Shop": "http://focallocal.org",
-  }
-
+  // add entries to container
   for(var title in entries) {
     if(entries.hasOwnProperty(title)) {
       var url = entries[title];
@@ -50,12 +51,15 @@
       container.appendChild(entry);
     }
   }
+
+  // las entry gets special css treament
   container.lastChild.className += " last-child";
 
+  /* LOAD CSS */
   // materialize grid stuff
-
   loadCss("https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css");
 
+  // get URL to folder, from which this script was loaded and include fl-menu.css
   var scriptEls = document.getElementsByTagName( 'script' );
   var thisScriptEl = scriptEls[scriptEls.length - 1];
   var scriptPath = thisScriptEl.src;
