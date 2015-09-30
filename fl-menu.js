cad118bb7e680b507537647c8cd50132916bbe36
hesslau <hesslau@gmail.com>
2015-09-30 01:57:45 -0700

    lots of changes; depends on responsive-nav

--- fl-menu.js
+++ fl-menu.js
@@ -2,9 +2,9 @@
 
     // set the entries { title: url }
     var entries = {
-      "Positive Social Action Hero Missions": "http://focallocal.org",
-      "Community News": "http://focallocal.org",
-      "What is the Focallocal Movement?": "http://focallocal.org",
+      "About FL": "http://focallocal.org",
+      "Profile": "http://focallocal.org",
+      "FL News": "http://focallocal.org",
       "Events": "http://focallocal.org",
       "Shop": "http://focallocal.org",
     }
@@ -18,36 +18,53 @@
     document.getElementsByTagName( "head" )[0].appendChild( link );
   }
 
+   /*
+   <nav class="nav-collapse">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  */
   var createEntry = function(title, href) {
-    var div = document.createElement("div");
-    var a = document.createElement("a");
-    a.href = href;
-    a.innerHTML = title;
-    div.appendChild(a);
-    return div;
+    var listElement = document.createElement("li");
+    var link = document.createElement("a");
+    link.href = href;
+    link.innerHTML = title;
+    listElement.appendChild(link);
+    return listElement;
   }
 
+  //var getMenuItems = function () { return document.querySelectorAll('.menu-item'); }
+
   // add the fl-menu to the body as first child
-  var menu = document.createElement("div");
-  menu.id = "fl-menu";
-  document.body.insertBefore(menu, document.body.firstChild);
+  var wrapper = document.createElement("div");
+  var menu = document.createElement("nav");
+  var container = document.createElement("ul");
+  var logo = document.createElement("a");
+
+  wrapper.id = "fl-menu";
+  logo.className = "menu-logo";
+  logo.innerHTML = "Focallocal";
+  menu.className = "nav-collapse";
+
+  wrapper.appendChild(logo);
+  document.body.insertBefore(wrapper, document.body.firstChild);
+  menu.appendChild(container);
+  wrapper.appendChild(menu);
 
   // add container for links to the menu
-  var container = document.createElement("div");
-  container.className = "menu-container row";
-  menu.appendChild(container);
 
   // add the logo
-  var logo = createEntry("Focallocal", "http://focallocal.org");
-  logo.className = "menu-logo col s1";
-  container.appendChild(logo);
 
   // add entries to container
   for(var title in entries) {
     if(entries.hasOwnProperty(title)) {
       var url = entries[title];
       var entry = createEntry(title, url);
-      entry.className = "menu-item col s2";
+      entry.className = "";
       container.appendChild(entry);
     }
   }
@@ -55,15 +72,15 @@
   // las entry gets special css treament
   container.lastChild.className += " last-child";
 
-  /* LOAD CSS */
-  // materialize grid stuff
-  loadCss("https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css");
+  // make navigation responsive
+  var nav = responsiveNav(".nav-collapse");
 
+  /* LOAD CSS */
   // get URL to folder, from which this script was loaded and include fl-menu.css
   var scriptEls = document.getElementsByTagName( 'script' );
   var thisScriptEl = scriptEls[scriptEls.length - 1];
   var scriptPath = thisScriptEl.src;
   var scriptFolder = scriptPath.substr(0, scriptPath.lastIndexOf( '/' )+1 );
-  loadCss(scriptFolder + "fl-menu.css");
+  loadCss(scriptFolder + "fl-menu.all.css");
 
 })();
