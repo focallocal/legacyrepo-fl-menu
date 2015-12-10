0353f8a0557e21d749993c62b2a04cc7908aae71
hesslau <hesslau@gmail.com>
2015-12-10 08:23:52 -0800

    scoped materilaize css rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uncss = require('gulp-uncss');
+var scopeCss = require("gulp-scope-css");
 
 gulp.task('default', function () {
   gulp.src('./sass/index.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(uncss({
+            html: ['fl-menu.html']
+        }))
+    .pipe(scopeCss('#fl-menu'))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./css'));
 });
 
