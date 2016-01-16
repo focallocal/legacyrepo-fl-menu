a51ef9818fe50d18489cba69fe34bbd6bbfd7515
hesslau <hesslau@gmail.com>
2016-01-16 13:42:09 -0600

    organize gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var uncss = require('gulp-uncss');
 var scopeCss = require("gulp-scope-css");
 var mustache = require("gulp-mustache");
 
-gulp.task('default', function () {
+gulp.task('default', ['sass','mustache']);
+
+gulp.task('sass', function() {
   gulp.src('./sass/index.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(uncss({
@@ -13,10 +15,12 @@ gulp.task('default', function () {
     .pipe(scopeCss('#fl-menu'))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./css'));
+})
 
+gulp.task('mustache', function() {
   gulp.src("./fl-menu.html")
-  	.pipe(mustache())
-  	.pipe(gulp.dest("./dist/"));
+    .pipe(mustache())
+    .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task('sass:watch', function () {
