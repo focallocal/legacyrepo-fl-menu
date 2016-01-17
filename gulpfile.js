9ead5ee68d3d9b5f73477493284a008b708eaedf
hesslau <hesslau@gmail.com>
2016-01-17 10:56:56 -0600

    materialize is compiled seperately to clean css from unused styles. to do a complete build, run the gulp tasks materialize, sass and mustache in that order (todo: figure out how gulp task dependencies work)

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,39 @@ var sass = require('gulp-sass');
 var uncss = require('gulp-uncss');
 var scopeCss = require("gulp-scope-css");
 var mustache = require("gulp-mustache");
+var rename = require("gulp-rename");
 
 //gulp.task('default', ['']);
 
 gulp.task('sass', function() {
   gulp.src('./sass/index.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(uncss({
-            html: ['fl-menu.html']
-        }))
+    .pipe(sass().on('error', sass.logError))
     .pipe(scopeCss('#fl-menu'))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('materialize', function() {
+
+  // first make sure that all used icons are present in html file
+  gulp.src("./fl-menu.html")
+    .pipe(mustache("fl-menu.json"))
+    .pipe(gulp.dest("./dist/"));
+
+  // then compile materialize scss and clean using compiled html
+  gulp.src('./sass/materialize/materialize.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(uncss({
+                  "html": ['./dist/fl-menu.html']
+                }))
+    .pipe(rename('_materialize.css'))
+    .pipe(gulp.dest('./sass/'));
+});
+
+gulp.task('compress', function() {
+  uncss(['fl-menu.html'], function (error, output) {
+    gulp.dest('./dist/fl-menu.html');
+  });
 })
 
 gulp.task('mustache', function() {
