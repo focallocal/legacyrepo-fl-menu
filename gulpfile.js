d635763f7b9771f43e55228460cb323cef1200e9
hesslau <hesslau@gmail.com>
2016-01-17 11:16:32 -0600

    move mustache files to mustache folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,24 @@ var uncss = require('gulp-uncss');
 var scopeCss = require("gulp-scope-css");
 var mustache = require("gulp-mustache");
 var rename = require("gulp-rename");
+var runSequence = require('run-sequence')
 
-//gulp.task('default', ['']);
+gulp.task('default', function(callback) {
+  runSequence('mustache','materialize','sass','mustache',callback);
+})
 
 gulp.task('sass', function() {
   gulp.src('./sass/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(scopeCss('#fl-menu'))
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./mustache/'));
 });
 
 gulp.task('materialize', function() {
 
   // first make sure that all used icons are present in html file
-  gulp.src("./fl-menu.html")
-    .pipe(mustache("fl-menu.json"))
-    .pipe(gulp.dest("./dist/"));
+  // task 'mustache' has to be run before this
 
   // then compile materialize scss and clean using compiled html
   gulp.src('./sass/materialize/materialize.scss')
@@ -32,18 +33,13 @@ gulp.task('materialize', function() {
     .pipe(gulp.dest('./sass/'));
 });
 
-gulp.task('compress', function() {
-  uncss(['fl-menu.html'], function (error, output) {
-    gulp.dest('./dist/fl-menu.html');
-  });
-})
-
 gulp.task('mustache', function() {
     // declaring a task dependency didn't work.
     // https://github.com/gulpjs/gulp/blob/master/docs/API.md#async-task-support
     // so i have to run gulp mustache everytime after gulp sass
-    gulp.src("./fl-menu.html")
+    gulp.src("./mustache/fl-menu.mustache")
       .pipe(mustache("fl-menu.json"))
+      .pipe(rename('fl-menu.html'))
       .pipe(gulp.dest("./dist/"));
 });
 
